d895ea8c673494ec1309db4abdf006792505b52f
Chemaxxxx <josemangt6@gmail.com>
2025-11-07 17:00:54 +0100

    Add files via upload

---
+++ Eco.java
@@ -0,0 +1,13 @@
+package u3;
+
+public class Eco {
+	public static void eco (int numero) {
+		for(int i = 1; i<= numero; i++) {
+			System.out.println("Eco...");
+		}
+	}
+	public static void main(String[] args) {
+		eco(5);
+	}
+
+}
